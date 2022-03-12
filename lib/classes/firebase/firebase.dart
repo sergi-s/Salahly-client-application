0b884ad28e61dadc6ff9d18f2104446b0c8773f4
Mohamed Hesham <70543887+Moh1medHesh1m@users.noreply.github.com>
2022-03-12 14:05:10 +0200

    signup_auth

--- lib/classes/firebase/firebase.dart
+++ lib/classes/firebase/firebase.dart
@@ -1,19 +1,50 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:slahly/abstract_classes/authentication.dart';
 import 'package:slahly/classes/models/client.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-class FirebaseAuth extends Authentication{
-
+class FirebaseAuth extends Authentication {
   @override
   void login(Client client) {
     // TODO: Magdy
-
   }
-
 
   @override
-  void signup(Client client) {
-    // TODO: H
+  Future<bool> signup(Client client) async {
+    DatabaseReference usersRef =
+        FirebaseDatabase.instance.reference().child("user");
 
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+
+    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+    final User firebaseUser =
+        (await _firebaseAuth.createUserWithEmailAndPassword(
+                email: client.email, password: client.password))
+            .catchError((errMsg) {
+      print(errMsg);
+      return false;
+    }).user;
+    if (firebaseUser != null) {
+      //user created
+      Map userDataMap = {
+        "name": client.name,
+        "email": client.email,
+        "birthday": client.birthday,
+        "createdDate": client.createdDate,
+        "sex": client.sex,
+        "type": client.type,
+        "avatar": client.avatar,
+        "address": client.address,
+        "phoneNumber": client.phoneNumber,
+        "loc": client.loc
+      };
+      usersRef.child(firebaseUser.uid).set(userDataMap);
+      return true;
+    }
+    return false;
   }
-
 }
