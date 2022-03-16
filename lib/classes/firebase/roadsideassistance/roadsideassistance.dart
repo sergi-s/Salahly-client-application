3cf78f8778c0237a9e326af481b6536b50c73cee
Mohamed Foula <mohamed.m.foula@gmail.com>
2022-03-16 23:44:21 +0200

    nearby_locations with test in homescreen

--- lib/classes/firebase/roadsideassistance/roadsideassistance.dart
+++ lib/classes/firebase/roadsideassistance/roadsideassistance.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:slahly/classes/models/location.dart';
 import 'package:slahly/classes/models/mechanic.dart';
 import 'package:slahly/classes/models/towProvider.dart';
@@ -7,19 +8,66 @@ import '../../../main.dart';
 
 class RSA {
   RSA_state state = RSA_state.created;
-  late CustomLocation location;
+  late CustomLocation location; // lazm yt2sm le long w lat
   late String RSA_id;
   late String userID;
   late TowProvider towProvider;
   late Mechanic mechanic;
   late DateTime estimatedTime;
-  late List<Mechanic> nearbyMechanics;
-  late List<TowProvider> nearbyProviders;
+  late List<Mechanic> nearbyMechanics; // not included in FB
+  late List<TowProvider> nearbyProviders; // not included in FB
 
   bool requestNearbyMechanics(){
     //uses location get nearby mechs
     nearbyMechanics = [];
     return false;
+  }
+
+  Future _checkLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      print("Location services are disabled.");
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        print('Location permissions are denied');
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      print('Location permissions are permanently denied, we cannot request permissions.');
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    return true;
+  }
+
+  Future<CustomLocation> getUserLocation() async {
+    _checkLocationPermission().onError((error, stackTrace)  {
+      print(error);
+      return null;
+    });
+    print("sad");
+    Position pos = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    return CustomLocation(longitude: pos.longitude,latitude: pos.latitude);
+    print("sad");
   }
 
   bool requestNearbyProviders(){
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -2,121 +2,82 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:slahly/classes/firebase/firebase.dart';
-import 'package:slahly/classes/models/client.dart';
-import 'package:slahly/main.dart';
+import 'package:flutter_geofire/flutter_geofire.dart';
+import 'package:go_router/go_router.dart';
 import 'package:http/http.dart' as http;
+import 'package:slahly/classes/firebase/nearbylocations.dart';
+import 'package:slahly/classes/firebase/roadsideassistance/roadsideassistance.dart';
+import 'package:slahly/classes/models/location.dart';
+import 'package:slahly/main.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static final routeName = "/homescreen";
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late CustomLocation userLoc;
+  NearbyLocations geo = NearbyLocations();
+  @override
+  void initState() {
+
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         height: double.infinity,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(onPressed: _sendMessage, child: Text("Test send msg")),
-              ElevatedButton(onPressed: ()async{
-                print(await _readmsg().toString());
-              }, child: Text("Test read msg")),
-              ElevatedButton(onPressed: ()async{
-                signupAccount();
-              }, child: Text("Sign up test")),
-              ElevatedButton(onPressed: ()async{
-                login();
-              }, child: Text("login test")),
-              ElevatedButton(onPressed: ()async{
-                httptest();
-              }, child: Text("http test")),
-            ],
-          ),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // ElevatedButton(
+            //     onPressed: ()async{
+            //       geo.addNearbyMechanics();
+            //     },
+            //     child: Text('Add mechanics')),
+            // ElevatedButton(
+            //     onPressed: ()async{
+            //       geo.addNearbyProviders();
+            //     },
+            //     child: Text('Add providers')),
+
+            ElevatedButton(
+                onPressed: ()async{
+                  // await FirebaseAuth.instance.signOut();
+                   geo.getNearbyMechanics(31.206971, 29.919035,100);
+
+
+                  // print("Keys retrieved "+NearbyLocations.keysRetrieved.length.toString());
+                  // for(var k in NearbyLocations.keysRetrieved){
+                  //   print("WE RETREIVED: "+k);
+                  // }
+                },
+                child: Text('get nearby mechanics')),
+            ElevatedButton(
+                onPressed: ()async{
+                  // await FirebaseAuth.instance.signOut();
+                  geo.getNearbyProviders(31.206971, 29.919035,100);
+
+
+                  // print("Keys retrieved "+NearbyLocations.keysRetrieved.length.toString());
+                  // for(var k in NearbyLocations.keysRetrieved){
+                  //   print("WE RETREIVED: "+k);
+                  // }
+                },
+                child: Text('get nearby providers')),
+
+          ],
         ),
       ),
     );
   }
-  Future<DataSnapshot> _readmsg(){
-    final DatabaseReference messagesRef =  FirebaseDatabase.instance.ref("mod");
-    return messagesRef.get();
-  }
-  void _sendMessage() {
 
-    if (true) {
-      final message = Message("Hello test", DateTime.now());
-      saveMessage(message);
-    }
-  }
-  Future<bool> login() async {
-    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-    // TODO: Magdy
-    try {
-      final user = await _firebaseAuth.signInWithEmailAndPassword(
-          email: "mo@mo.mo", password: "momomo");
-      if (user != null) {
-        return true;
-      }
-    } catch (e) {
-      print(e);
-      return false;
-    }
-    return false;
-  }
-    Future signupAccount()async {
-    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-      final User? firebaseUser = (await _firebaseAuth
-          .createUserWithEmailAndPassword(
-          email: "mo@mo.mo", password: "momomo")
-          .catchError((errMsg) {
-        print(errMsg);
-        return false;
-      }))
-          .user;
 
-      if (firebaseUser != null) {
-        //user created
-        return true;
-      }
-      return false;
-
-  }
-  void httptest() async {
-    var url = Uri.parse("http://10.0.2.2:5001/salahny-6bfea/us-central1/helloWorld");
-    var response = await http.get(url);
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-  }
-//    ".read": "now < 1649368800000",  // 2022-4-8
-//    ".write": "now < 1649368800000",  // 2022-4-8
-
-  void saveMessage(Message message) {
-    // final DatabaseReference messagesRef =  FirebaseDatabase.instance.ref().child('messages');
-    final DatabaseReference messagesRef =  FirebaseDatabase.instance.ref();
-    messagesRef.child("messages").set({
-      'date': 'sergi',
-      'text': 'kokoko'
-    });
-    // messagesRef.push().set(message.toJson());
-    print("OK");
-  }
-  // Query getMessageQuery() {
-  //   return messagesRef;
-  // }
-}
-
-class Message {
-  final String text;
-  final DateTime date;
-
-  Message(this.text, this.date);
-
-  Message.fromJson(Map<dynamic, dynamic> json)
-      : date = DateTime.parse(json['date'] as String),
-        text = json['text'] as String;
-
-  Map<dynamic, dynamic> toJson() => <dynamic, dynamic>{
-    'date': date.toString(),
-    'text': text,
-  };
 }
