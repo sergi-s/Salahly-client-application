a00644590b1e759be7da5269afa84217487be7b0
Mahmoud-Megz <mahmoudmagdy.cs@gmail.com>
2022-04-03 10:55:10 +0200

    Magdyyy

--- lib/screens/viewcars.dart
+++ lib/screens/viewcars.dart
@@ -14,92 +14,124 @@ class ViewCars extends StatelessWidget {
   }
 }
 
-class CarCard extends StatefulWidget {
-
-
-  @override
-  _CarCardState createState() => _CarCardState();
-}
-
-class _CarCardState extends State<CarCard> {
-
-  var _addCard = 0;
-
-  void _incrementCard()
-  {
-    setState(() {
-      _addCard ++ ;
-    });
-  }
-
+class CarCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.only(left: 0.0, bottom: 8.0, right: 16.0),
+      decoration: new BoxDecoration(color: Colors.blue),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
 
-    return Scaffold(
-      appBar: AppBar(
+          new SizedBox(
+            width:0.0 ,
+            height: 35.0,
+          ),
 
-        title: Text("App Bar"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCard,
-        child: Icon(Icons.add),
-      ),
-      body:ListView.builder(
-          itemCount: _addCard,
-          itemBuilder: (context, index){
-            return Card(
-              child: Container(
+          new Card(
+            child: new Column(
+              children: <Widget>[
 
-                color:Colors.white12,
-                child: Column(
-                  children: [
-                    TextField (
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          labelText: "Car Model"
+                new Icon(Icons.invert_colors_on_sharp),
+                new Text("Chassis Number: SV30-01xxxx",
+                    style: new TextStyle(fontSize: 18.0)),
+                new Padding(
+                    padding: new EdgeInsets.all(7.0),
+                    child: new Row(
+                      children: <Widget>[
 
-                      ),
-                    ),
-                    TextField (
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          labelText: "Model Year"
-                      ),
-                    ),
-                    TextField (
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          labelText: "Number Plate"
-                      ),
-                    ),
-                    TextField (
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          labelText: "Color Pickes"
-                      ),
-                    ),
-                    TextField (
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          labelText: " Chassis Number"
-                      ),
-                    ),
-                  ],
-                ),
+                        new Padding(
+                          padding: new EdgeInsets.all(7.0),
+                          child: new Text(
+                            'Mg Car',
+                            style: new TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+
+                        new Padding(
+                          padding: new EdgeInsets.all(7.0),
+                          child: new Text('Number Plate: س ق ه | 2111',
+                              style: new TextStyle(fontSize: 18.0)),
+                        )
+                      ],
+                    ))
+              ],
+            ),
+          ),
+
+          new Card(
+            child: new Column(
+              children: <Widget>[
 
 
-              ),
-            );
-          }
+                new Text("Chassis Number: SV30-01xxxx",
+                    style: new TextStyle(fontSize: 18.0)),
+                new Padding(
+                    padding: new EdgeInsets.all(7.0),
+                    child: new Row(
+                      children: <Widget>[
+
+                        new Padding(
+                          padding: new EdgeInsets.all(7.0),
+                          child: new Text(
+                            'BYD',
+                            style: new TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+
+                        new Padding(
+                          padding: new EdgeInsets.all(7.0),
+                          child: new Text('Number Plate: س ق ه | 2111',
+                              style: new TextStyle(fontSize: 18.0)),
+
+                        ),
+
+                        new Icon(Icons.invert_colors_on_sharp),
+                      ],
+                    ))
+              ],
+            ),
+          ),
+          new Card(
+            child: new Column(
+              children: <Widget>[
+
+
+                new Text("Chassis Number: SV30-01xxxx",
+                    style: new TextStyle(fontSize: 18.0)),
+                new Padding(
+                    padding: new EdgeInsets.all(7.0),
+                    child: new Row(
+                      children: <Widget>[
+
+                        new Padding(
+                          padding: new EdgeInsets.all(7.0),
+                          child: new Text(
+                            'Bmw',
+                            style: new TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+
+                        new Padding(
+                          padding: new EdgeInsets.all(7.0),
+                          child: new Text('Number Plate: س ق ه | 2111',
+                              style: new TextStyle(fontSize: 18.0)),
+
+                        ),
+                        new Icon(Icons.invert_colors_on_sharp),
+                      ],
+                    ))
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
-
-
-
 }
-
 
 
 
