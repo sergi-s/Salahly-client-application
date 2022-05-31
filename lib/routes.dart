fe8cd2515e7000a37f25e4b833fe683d8b1cfcc5
Mohamed Foula <mohamed.m.foula@gmail.com>
2022-05-31 15:56:52 +0200

    made a function that works well with function stop listener, the initial route has the test screen that simulates these functions, you can integrate them with the rest of the project, note: if you are willing to use Foula function in nearbylocations please dont delete the old one and leave it as it was and create a new function, note: Foula function doesnt insert in database and only deals with local data

--- lib/routes.dart
+++ lib/routes.dart
@@ -28,6 +28,7 @@ import 'package:slahly/screens/roadsideassistance/rsaconfirmationScreen.dart';
 import 'package:slahly/screens/roadsideassistance/searching_mechanic_provider_screen.dart';
 import 'package:slahly/screens/splashScreen/splashscreen.dart';
 import 'package:slahly/screens/switchLanguage.dart';
+import 'package:slahly/screens/test_screens/test_screen_foula_nearbylocations.dart';
 import 'package:slahly/screens/test_screens/test_user_SM.dart';
 import 'package:slahly/screens/test_screens/testscreen_foula.dart';
 import 'package:slahly/screens/testscreenyoyo.dart';
@@ -47,7 +48,7 @@ import 'screens/roadsideassistance/arrival.dart';
 
 class Routing {
   get router => GoRouter(
-        initialLocation: CheckLogin.routeName,
+        initialLocation: TestScreenFoulaNearbyLocations.routeName,
 
         // initialLocation: RequestFinalScreen.routeName,
         routes: <GoRoute>[
@@ -293,6 +294,10 @@ class Routing {
           GoRoute(
             path: TestScreenAya.routeName,
             builder: (context, state) => TestScreenAya(),
+          ),
+          GoRoute(
+            path: TestScreenFoulaNearbyLocations.routeName,
+            builder: (context, state) => TestScreenFoulaNearbyLocations(),
           ),
         ],
       );
